24d1ab46ef9307475869e912e3a54074a0b78605
Deeksha <deeksha.sharma25@gmail.com>
2015-08-22 17:53:21 -0700

    First commit

---
+++ src/main/java/RestApplication.java
@@ -0,0 +1,18 @@
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+@ApplicationPath("/rest")
+public class RestApplication extends Application {
+
+    final Set<Class<?>> classes = new HashSet<Class<?>>();
+
+    @Override
+    public Set<Class<?>> getClasses() {
+        classes.add(RestEasyHelloWorldService.class);
+        return Collections.unmodifiableSet(classes);
+    }
+
+}
---
+++ src/main/java/RestEasyHelloWorldService.java
@@ -0,0 +1,13 @@
+import javax.ws.rs.*;
+import javax.ws.rs.core.Response;
+
+@Path("data")
+public class RestEasyHelloWorldService {
+
+    @GET
+    @Path("{moduleId}")
+    public Response responseMessage(@PathParam("moduleId") String moduleId, @QueryParam("value") String value,@QueryParam("type") String type){
+        String response = "BreadBoardId = "+moduleId+ " & Temperature = " + value+ " & Type = "+type;
+        return Response.status(200).entity(response).build();
+    }
+}
